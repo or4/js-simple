e576d5bac2038d36e272da3404547a32fe56bf80
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-10-04 15:49:24 +0700

    anagrama

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@
 // import { Path } from 'ramda/Path';
 // import { Dfs } from 'graph/Dfs';
 // import { Bfs } from 'graph/Bfs';
-import { Deikstra } from 'graph/Deikstra';
+// import { Deikstra } from 'graph/Deikstra';
+import { calcAnagram } from 'other/anagrama';
+
 import * as React from 'react';
+
+console.log('test',
+  calcAnagram(['asdf', 'fsad'])
+);
+
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-        <Deikstra />
+        Test
       </div>
     );
   }
---
+++ src/other/anagrama.ts
@@ -0,0 +1,7 @@
+export const isAnagrama = (s1: string, s2: string) => s1.split('').sort().join('') === s2.split('').sort().join('');
+
+export const calcAnagram = (arr: string[]) => {
+
+
+  // return output;
+};
